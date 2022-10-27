eb6e7f52e453973abb51fffce85e4d72a9f2cf8d
Mirae Kim <miraekim1031@gmail.com>
2022-10-27 01:52:01 -0400

    댓글 수정 기능

---
+++ src/features/comments/GetComment.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import styled from "styled-components";
+import { __modifyComment } from "../../redux/modules/commentSlice";
+
+const GetCommnet = ({ comment }) => {
+   const [isEdit, setEdit] = useState(false);
+   const [input, setInput] = useState();
+   const dispatch = useDispatch();
+
+   const onModifyCommentHandler = () => {
+    setEdit(!isEdit);
+    // if (input.trim() === '') {
+    //    return alert('내용을 입력해주세요');
+    // }
+    dispatch(__modifyComment({ ...comment, comment: input }));
+ };
+
+
+   return (
+      <>
+         <CommentBlock>
+            <Comments>
+            <LoginId>{comment?.loginId}</LoginId>
+            <Comment>
+               {!isEdit ? (
+                  <>{comment?.comment}</>
+               ) : (
+                  <input
+                     value={input}
+                     placeholder={`${comment?.comment}`}
+                     onChange={(e) => {
+                        setInput(e.target.value);
+                     }}></input>
+               )}
+            </Comment>
+            <Btns>
+               {!isEdit ? (
+                  <button onClick={() => setEdit(true)}>수정</button>
+               ) : (
+                  <button onClick={onModifyCommentHandler}>완료</button>
+               )}
+               <button
+               //   onClick={onDeleteCommentHandler}
+               >
+                  삭제
+               </button>
+            </Btns>
+            </Comments>
+         </CommentBlock>
+      </>
+   );
+};
+
+export default GetCommnet;
+
+const CommentBlock = styled.div`
+   margin-top: 20px;
+`;
+
+const LoginId = styled.div`
+   float: left;
+   min-width: 100px;
+   height: 25px;
+   display: flex;
+   text-align: center;
+   justify-content: center;
+   align-items: center;
+   border-bottom: 1px solid #f32b2b;
+   margin-right: 20px;
+`;
+
+const Comment = styled.div`
+   display: flex;
+   text-align: center;
+   align-items: center;
+   height: 25px;
+   min-width: 380px;
+`;
+
+const Comments = styled.div`
+   margin-bottom: 5px;
+   display: flex;
+`;
+
+const Btns = styled.div``;
--- src/features/comments/getComments.jsx
+++ src/features/comments/getComments.jsx
@@ -2,7 +2,11 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
-import { __getComments, __modifyComment } from '../../redux/modules/commentSlice';
+import {
+   __getComments,
+   __modifyComment,
+} from '../../redux/modules/commentSlice';
+import GetComment from './GetComment';
 
 const GetComments = () => {
    const dispatch = useDispatch();
@@ -14,8 +18,6 @@ const GetComments = () => {
    const { isLoading, error, comments } = useSelector((state) => state.comment);
    //    const num = comments.length;
    //    console.log(num);
-   const [isEdit, setEdit] = useState(false);
-   const [input, setInput] = useState();
 
    if (isLoading) {
       return <div>로딩 중....</div>;
@@ -25,76 +27,21 @@ const GetComments = () => {
       return <div>{error.message}</div>;
    }
 
-   const onModifyCommentHandler = () => {
-    setEdit(!isEdit);
-    if (input.trim()==='') {
-        return alert ('내용을 입력해주세요');
-    }
-    dispatch(__modifyComment({...comments, comment: input}));
-   }
+//    const onModifyCommentHandler = () => {
+//       setEdit(!isEdit);
+//       if (input.trim() === '') {
+//          return alert('내용을 입력해주세요');
+//       }
+//       dispatch(__modifyComment({ ...comments, comment: input }));
+//    };
 
    return (
-      <CommentBlock>
+      <>
          {comments.map((comment) => (
-            <Comments key={comment?.commentId}>
-               <LoginId>{comment?.loginId}</LoginId>
-               <Comment>
-                {!isEdit? (<>{comment?.comment}</>
-                ) : (
-                    <input
-                    value={input}
-                    placeholder={`{comment?.comment}`}
-                    onChange={(e) => {
-                        setInput(e.target.value);
-                    }}></input>
-                ) }
-                </Comment>
-               <Btns>
-                  {!isEdit ? (
-                     <button onClick={() => setEdit(true)}>수정</button>
-                  ) : (
-                     <button onClick={onModifyCommentHandler}>
-                        저장</button>
-                  )}
-                  <button
-                //   onClick={onDeleteCommentHandler}
-                  >삭제</button>
-               </Btns>
-            </Comments>
+            <GetComment key={comment.commentId} comment={comment} />
          ))}
-      </CommentBlock>
+      </>
    );
 };
 
 export default GetComments;
-
-const CommentBlock = styled.div`
-   margin-top: 20px;
-`;
-
-const LoginId = styled.div`
-   float: left;
-   min-width: 100px;
-   height: 25px;
-   display: flex;
-   text-align: center;
-   justify-content: center;
-   align-items: center;
-   border-bottom: 1px solid #f32b2b;
-   margin-right: 20px;
-`;
-
-const Comment = styled.div`
-   display: flex;
-   text-align: center;
-   align-items: center;
-   height: 25px;
-   min-width: 380px;
-`;
-
-const Comments = styled.div`
-   margin-bottom: 5px;
-   display: flex;
-`;
-
-const Btns = styled.div``;
