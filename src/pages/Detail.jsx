ca8c99b703a1af48e6b34358d1bac245a059dab5
Forza5 <ganp433@gmail.com>
2022-10-27 19:55:38 +0900

    댓글 수정

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -2,31 +2,28 @@ import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import {
-
-   __deletePost,
-   __getPost,
-   __modifyPost,
-} from '../redux/modules/postSlice';
-import Layout from '../components/Layout';
-import styled from 'styled-components';
-import { __getPosts } from '../redux/modules/postsSlice';
-import GetComments from '../features/comments/GetComments';
-import AddComment from '../features/comments/AddComment';
+  __deletePost,
+  __getPost,
+  __modifyPost,
+} from "../redux/modules/postSlice";
+import Layout from "../components/Layout";
+import styled from "styled-components";
+import { __getPosts } from "../redux/modules/postsSlice";
+import GetComments from "../features/comments/getComments";
+import AddComment from "../features/comments/AddComment";
 
 const Detail = () => {
-   const dispatch = useDispatch();
-   useEffect(() => {
-      dispatch(__getPost(id.postId));
-   }, [dispatch]);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(__getPost(id.postId));
+  }, [dispatch]);
 
-   
-   const id = useParams();
-   const [isEdit, setEdit] = useState(false);
-   const ids = localStorage.getItem('loginId');
-   const [input, setInput] = useState();
-   const { isLoading, error, post } = useSelector((state) => state.post);
-   const navigate = useNavigate();
-
+  const id = useParams();
+  const [isEdit, setEdit] = useState(false);
+  const ids = localStorage.getItem("loginId");
+  const [input, setInput] = useState();
+  const { isLoading, error, post } = useSelector((state) => state.post);
+  const navigate = useNavigate();
 
   if (isLoading) {
     return <div>로딩 중....</div>;
@@ -36,68 +33,69 @@ const Detail = () => {
     return <div>{error?.message}</div>;
   }
 
-   const onModifyHandler = () => {
-      setEdit(!isEdit);
-      if (input.trim() === '') {
-         return alert('내용을 입력해주세요');
-      }
-      dispatch(__modifyPost({ ...post, content: input }));
-   };
+  const onModifyHandler = () => {
+    setEdit(!isEdit);
+    if (input.trim() === "") {
+      return alert("내용을 입력해주세요");
+    }
+    dispatch(__modifyPost({ ...post, content: input }));
+  };
 
-   const onDeleteHandler = () => {
-      dispatch(__deletePost(id.postId));
-      navigate('/');
-   };
+  const onDeleteHandler = () => {
+    dispatch(__deletePost(id.postId));
+    navigate("/");
+  };
 
-   return (
-      <Layout>
-         <PostView>
-            <Btns>
-               {ids === post?.loginId ? (
-                  <div>
-                     {!isEdit ? (
-                        <button onClick={() => setEdit(true)}>수정</button>
-                     ) : (
-                        <button onClick={onModifyHandler}>완료</button>
-                     )}
-                     <button onClick={onDeleteHandler}>삭제</button>
-                  </div>
-               ) : null}
-            </Btns>
-
+  return (
+    <Layout>
+      <PostView>
+        <Btns>
+          {ids === post?.loginId ? (
             <div>
-               {post?.typeofpet} / {post?.category} / {post?.subcategory}{' '}
+              {!isEdit ? (
+                <button onClick={() => setEdit(true)}>수정</button>
+              ) : (
+                <button onClick={onModifyHandler}>완료</button>
+              )}
+              <button onClick={onDeleteHandler}>삭제</button>
             </div>
-            <div>
-               제조사: {post?.maker} 제품명: {post?.product}
-            </div>
-            <div>{post?.loginId}</div>
-            <div>
-               <h3>{post?.title}</h3>
-            </div>
-            {!isEdit ? (
-               <div>{post?.content}</div>
-            ) : (
-               <ModifyInput
-                  //  ref = {inputRef}
-                  value={input}
-                  placeholder={`${post?.content}`}
-                  onChange={(e) => {
-                     setInput(e.target.value);
-                  }}></ModifyInput>
-            )}
-            {/* <div>{post?.content}</div> */}
-            <ImgView>
-               <ImgBlock src={post?.photo} />
-            </ImgView>
-         </PostView>
-         <CommentView>
-            <AddComment />
-            <GetComments />
-         </CommentView>
-      </Layout>
-   );
+          ) : null}
+        </Btns>
 
+        <div>
+          {post?.typeofpet} / {post?.category} / {post?.subcategory}{" "}
+        </div>
+        <div>
+          제조사: {post?.maker} 제품명: {post?.product}
+        </div>
+        <div>{post?.loginId}</div>
+        <div>
+          <h3>{post?.title}</h3>
+        </div>
+        {!isEdit ? (
+          <div>{post?.content}</div>
+        ) : (
+          <ModifyInput
+            //  ref = {inputRef}
+            value={input}
+            placeholder={`${post?.content}`}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+          ></ModifyInput>
+        )}
+        {/* <div>{post?.content}</div> */}
+        <ImgView>
+          <ImgBlock src={post?.photo} />
+        </ImgView>
+      </PostView>
+      <CommentView>
+        <label>댓글</label>
+        <AddComment />
+        <GetComments />
+      </CommentView>
+    </Layout>
+  );
 };
 
 export default Detail;
@@ -111,8 +109,8 @@ const PostView = styled.div`
 `;
 
 const CommentView = styled.div`
-  background-color: #e3e0e1;
-  width: 600px;
+  width: 70%;
+  margin: 0 auto;
   border-radius: 10px;
   padding: 20px;
 `;
--- src/redux/modules/loginSlice.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
-
-const LogIn = () => {
-  const navigate = useNavigate();
-};
